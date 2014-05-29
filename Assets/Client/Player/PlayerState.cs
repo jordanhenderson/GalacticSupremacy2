5dcc95d5e7ed54c71c4177234e5cab071fdae862
wombat9000 <stone.bastian@gmail.com>
2014-05-29 17:58:10 +1000

    Added initial code.

---
+++ Assets/Client/Player/PlayerState.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using gsFramework;
+
+// server side class, needs 1 instance per player
+public class PlayerState : MonoBehaviour {
+	
+	public int id;
+	public float incomeTime = 1f;
+	public int income = 10;
+	public int credits = 10;
+	//public SolReg[] regions;
+
+	void Awake () {
+		// keep this object instance when transitioning between scenes.
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	void Start () {
+		// in incomeTime intervals, run "addIncome"
+		InvokeRepeating("computeIncome", incomeTime, incomeTime);
+		InvokeRepeating("addIncome", incomeTime, incomeTime);
+	}
+	
+	void Update () {
+	}
+
+	/* computeIncome can get called by the client to request a recomputation
+	 * of the income stat. 
+	 *
+	 */
+	public void computeIncome () {
+		// find all regions where SolReg.owner == player.id
+		income = 0;
+		for (int i = 0; i < generatePlanets.numRegions; ++i) {
+			if (generatePlanets.regions[i].owner == id) {
+				income += generatePlanets.regions[i].income;
+				for (int j = 0; j < generatePlanets.regions[i].slots; ++j) {
+					income += generatePlanets.regions[i].buildings[j].income;	
+				}
+			}
+		}
+	}
+
+	/* Increments the players credits by his current income.
+	 */
+	void addIncome () {
+		credits += income;
+	}
+
+	public int getIncome () {
+		return income;
+	}
+
+	public int getCredits () {
+		return credits;
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-GalacticSupremacy2
-==================
+GalacticSupremacy
+=================
+
+DECO3800 Galactic Supremacy Group Development
+
+All our stuff is under the assets at the moment
+
+otherAssets foler is where all the graphical stuff goes
+
+view is where the classes for all the things we can see goes, i.e. ships, solar regions, links, menus
+
+controller is where all the backend stuff goes, i.e. determining battle outcomes, maybe talking to the server...
+-- Server files have now been added. At this stage, all logic should end up in server/core/process.lua@handle_request().
+-- Code may need to be cleaned/clarified, things are a little messy still.
+
+
+This is all one (empty) unity project right now that we can add bits to here and there.  You should be able to download (called cloning in github) and import it into Unity
