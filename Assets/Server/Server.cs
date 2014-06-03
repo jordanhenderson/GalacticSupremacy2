d3283d447a727914e0ab2e51acfca12e94a10ff2
jordanhenderson <jordan.henderson@uqconnect.edu.au>
2014-06-03 16:20:19 +1000

    Update Server.cs
    Move region code to individual regions.

--- Assets/Server/Server.cs
+++ Assets/Server/Server.cs
@@ -6,17 +6,28 @@ using SimpleJSON;
 public enum ServerAction {
 	ACTION_SEND_CHAT,
 	ACTION_GET_CHAT,
-	ACTION_GET_GAMESTATE,
-	ACTION_CONSTRUCT_BUILDING
+	ACTION_GET_INITIALSTATE,
+	ACTION_GET_REGION,
+	ACTION_UPDATE_REGION,
 };
+
 public class Server : Singleton<Server> {
 	protected Server() {}
-	public bool loaded = false;
-	public List<SolReg> regions = new List<SolReg>();
-	private void ParseGameState(byte[] data) {
+	private byte[] bytesFromString(string s) {
+		byte[] data = new byte[s.Length * sizeof(char)];
+		System.Buffer.BlockCopy (s.ToCharArray (), 0, data, 0, data.Length);
+		return data;
+	}
+	private string stringFromBytes(byte[] data) {
 		char[] chars = new char[data.Length / sizeof(char)];
 		System.Buffer.BlockCopy (data, 0, chars, 0, data.Length);
-		string data_str = new string (chars);
+		return new string (chars);	
+	}
+	
+	public bool loaded = false;
+	public List<SolReg> regions = new List<SolReg>();
+	private void ParseInitialState(byte[] data) {
+		string data_str = stringFromBytes(data);
 		//Deserialize the json array into gamedata.
 		JSONArray s = JSON.Parse (data_str).AsArray;
 		/* Naiive game-state systems read in the entire
@@ -30,6 +41,13 @@ public class Server : Singleton<Server> {
 		}
 		loaded = true;
 	}
+	
+	private void ParseGameState(byte[] data) {
+		JSONArray s = JSON.Parse(stringFromBytes(data)).AsArray;
+		foreach(JSONNode node in s) {
+			
+		} 
+	}
 
 	private string server_url = "http://deco3800-14.uqcloud.net/game.php";
 	private Hashtable header = new Hashtable ();
@@ -37,39 +55,17 @@ public class Server : Singleton<Server> {
 		header.Add ("Content-Type", "text/json");
 	}
 
-	private byte[] bytesFromString(string s) {
-		byte[] data = new byte[s.Length * sizeof(char)];
-		System.Buffer.BlockCopy (s.ToCharArray (), 0, data, 0, data.Length);
-		return data;
-	}
-
-	private IEnumerator UpdateGame() {
-		string request = "{action:" + ServerAction.ACTION_GET_GAMESTATE + "}";
+	private IEnumerator StartGame() {
+		string request = "{action:" + ServerAction.ACTION_GET_INITIALSTATE + "}";
 		byte[] data = bytesFromString (request);
 		header["Content-Length"] = data.Length;
 		WWW www = new WWW (server_url, data, header);
 		yield return www;
-		ParseGameState (www.bytes);
-
-	}
-
-	public IEnumerator SendRequest(ServerAction action) {
-		string request = "{action:" + action + ",}";
-		byte[] data = bytesFromString (request);
-		header["Content-Length"] = data.Length;
-		WWW www = new WWW (server_url, data, header);
-		yield return www;
-	
+		ParseInitialState(www.bytes);
 	}
 	
-	private int tick = 0; 
-	private int tick_step = 100; //How often to request the game state.
+
 	void Update() {
-		/* Apply the current game state to the locally running game. */
-		if (tick % tick_step == 0) {
-			StartCoroutine (UpdateGame ());
-			tick = 0;
-		}
-		tick++;
+		
 	}
 }
