5f8abdb0ff4e14ad62f624eb9dc04aaf81bb30fe
jordanhenderson <jordan.henderson@uqconnect.edu.au>
2014-06-03 16:06:36 +1000

    Update game_logic.php
    Modified game state to fit latest class structure.

--- server/include/game_logic.php
+++ server/include/game_logic.php
@@ -7,28 +7,33 @@ function get_chat($request) {
 
 }
 
+//Global gamestate object
+$gamestate = array();
 
-$gamestate = array(
-	//Initial game state array (SolReg positions etc).
-	array(
-		//sector id, planet id, xpos, zpos, scale, texture, owner, income, slots, array(connections)
-		array(0,0,15.6,2.9,0.5,1,0,10,3,array(1, 2)),
-		array(0,1,1.9,-8.5,0.2,4,0,14,2,array(0, 3)),
-		array(0,2,-8.2,-4.9,0.6,2,0,18,4,array(0)),
-		array(0,3,9.5,-6.7,0.1,3,0,4,3,array(1)),
-		array(0,4,-0.3,-1.24,0.3,3,0,7,5,array(5)),
-		array(0,5,-4,-14.3,0.7,1,0,24,7,array(6, 4)),
-		array(0,6,16,16.5,0.8,4,0,14,1,array(7, 5)),
-		array(0,7,9.6,14.36,0.8,3,0,29,2,array(8, 6)),
-		array(0,8,6.6,3.92,0.4,3,0,3,2,array(9, 7)),
-		array(0,9,-14.56,-14.7,0.5,1,0,11,4,array(8))
-	)
+function generate_gamestate() {
+	global $gamestate;
+	$gamestate = array(
+		//Initial game state array (SolReg positions etc). This should be generated/made persistent.
+		array(
+			//sector id, planet id, xpos, zpos, scale, texture, owner, income, slots, emptyslots, array(connections), array(buildings)
+			array(0,0,15.6,2.9,0.5,1,0,10,3,0,array(1, 2), array()),
+			array(0,1,1.9,-8.5,0.2,4,0,14,2,0,array(0, 3), array()),
+			array(0,2,-8.2,-4.9,0.6,2,0,18,4,0,array(0), array()),
+			array(0,3,9.5,-6.7,0.1,3,0,4,3,0,array(1), array()),
+			array(0,4,-0.3,-1.24,0.3,3,0,7,5,0,array(5), array()),
+			array(0,5,-4,-14.3,0.7,1,0,24,7,0,array(6, 4), array()),
+			array(0,6,16,16.5,0.8,4,0,14,1,0,array(7, 5), array()),
+			array(0,7,9.6,14.36,0.8,3,0,29,2,0,array(8, 6), array()),
+			array(0,8,6.6,3.92,0.4,3,0,3,2,0,array(9, 7), array()),
+			array(0,9,-14.56,-14.7,0.5,1,0,11,4,0,array(8), array())
+		);
 	
-);
+	);	
+}
 
 /*
 Return the game-state to the player. 
-This should be called every frame.
+This should only be called upon player connection.
 */
 
 function get_gamestate($request) {
