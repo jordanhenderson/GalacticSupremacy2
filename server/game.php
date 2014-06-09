779e92f6c2b84ca1b6196ac1a371fd117555c470
Jordan Henderson <jordan.henderson@ioflame.com>
2014-06-09 11:21:23 +0000

    Fixed server outputting default action.

--- server/game.php
+++ server/game.php
@@ -2,25 +2,26 @@
 define("IN_GAME", TRUE);
 require("include/game_logic.php");
 
-$ACTION_SEND_CHAT = 0;
-$ACTION_GET_CHAT = 1;
-$ACTION_GET_GAMESTATE = 2;
-$ACTION_PLAYER_EVENT = 3;
-
-
 $r = json_decode(file_get_contents('php://input'), true);
-$action = $r['action'];
-$request = $r['data'];
-
-switch($action) {
+function handle_request($r) {
+	$ACTION_SEND_CHAT = 0;
+	$ACTION_GET_CHAT = 1;
+	$ACTION_GET_GAMESTATE = 2;
+	$ACTION_PLAYER_EVENT = 3;
+	$action = $r['action'];
+	$request = $r['data'];
+	switch($action) {
 	case $ACTION_SEND_CHAT:
-		echo send_chat($request);
+		return send_chat($request);
 	case $ACTION_GET_CHAT:
-		echo get_chat($request);
+		return get_chat($request);
 	case $ACTION_GET_GAMESTATE:
-		echo get_gamestate($request);
+		return  get_gamestate($request);
 	case $ACTION_PLAYER_EVENT:
-		echo player_event($request);
+		return player_event($request);
+	}
 }
+echo handle_request($r);
+
 
 ?>
