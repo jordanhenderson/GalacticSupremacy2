b8a203617c99c83342ca7607a0d3e6d75de41283
Sarah Nelson <sarah.nelson1@uqconnect.edu.au>
2014-06-09 16:45:31 +1000

    Added login camera with rotation and skybox.

---
+++ Assets/Game/LoginCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoginCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (1.0f * Time.deltaTime, 1.0f * Time.deltaTime, 1.0f * Time.deltaTime);
+	}
+}
